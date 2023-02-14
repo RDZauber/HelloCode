3a41673b81094ddb1f17f0fff32e29da69267a40
Руслан Данилов <RDZauber@gmail.com>
2023-02-14 19:51:58 +0900

    EXample_Seminar01 Блок схема 1 задачи

---
+++ EXample_Seminar01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("START");
+int N;
+
+Console.WriteLine("Введите N");
+
+N = 5;
+
+int index;
+index = -N;
+
+while (index <= N)
+{
+    Console.Write(index + " ");
+    index = index + 1;
+
+}
+
+Console.WriteLine("END");
