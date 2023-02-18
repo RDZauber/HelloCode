259a611e15612e9c7e98b5b046f2ef49ae758c6a
Руслан Данилов <RDZauber@gmail.com>
2023-02-18 22:43:56 +0900

    Создана папка Work03 где размещена третья задача

---
+++ Example_Homework02/Work03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int DayNumbers = Convert.ToInt32(Console.ReadLine());
+if (DayNumbers == 6 || DayNumbers == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else if (DayNumbers < 1 || DayNumbers > 7)
+{
+   
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+    Console.WriteLine("Это не выходной день");
+}
+
