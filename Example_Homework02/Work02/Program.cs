dae0d089c4a3757f4c546e5498df2a942d7371f0
Руслан Данилов <RDZauber@gmail.com>
2023-02-18 21:55:53 +0900

    Создана папка Work02 где размещена вторая задача

---
+++ Example_Homework02/Work02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value >= 9999)
+{
+    int c = (value % 1000);
+    int result = c / 100;
+    Console.WriteLine(result);
+
+
+}
+else
+if (value >= 99)
+{
+    int c = value % 10;
+    Console.WriteLine(" Третья цифра значения ");
+    Console.WriteLine(c);
+}
+else
+{
+
+    Console.WriteLine("Третьей цифры нет в значении");
+}
