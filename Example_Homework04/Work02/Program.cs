d81879143db313c332d371a09f88c04cdc127372
Руслан Данилов <RDZauber@gmail.com>
2023-03-01 19:47:19 +0900

    Создана папка work04

---
+++ Example_Homework04/Work02/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+ int data(string text)
+{
+    System.Console.Write(text);
+    string numbers = System.Console.ReadLine();
+    int result = int.Parse(numbers);
+    return result;
+
+}
+
+int SumAll(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = data("Введите число: ");
+System.Console.WriteLine($" Сумма всех чисел, числа {number} = {SumAll(number)}");
